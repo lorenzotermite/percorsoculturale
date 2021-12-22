1d89959d07e4a292ddbaa7a6350d0157efe9c654
fabiocccc <contefabio98@gmail.com>
2021-12-22 10:47:25 +0100

    mappa

---
+++ app/src/main/java/com/example/mappa/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mappa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Mappe.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
